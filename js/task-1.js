402aa14fbd0e802d391891032c829e2bc6d0f908
Viktor Momotenko <viktor.momotenko@gmail.com>
2024-06-06 01:37:09 +0100

    task-1 done (clearly)

--- js/task-1.js
+++ js/task-1.js
@@ -1,14 +1,10 @@
 const categoryList = document.querySelector("#categories");
-
 const categoryItems = categoryList.querySelectorAll(".item");
-
 console.log(`Number of categories: ${categoryItems.length}`);
 
 categoryItems.forEach((item) => {
   const categoryTitle = item.querySelector("h2").textContent;
-
   const categoryElements = item.querySelectorAll("li");
-
   console.log(`Category: ${categoryTitle}`);
   console.log(`Elements: ${categoryElements.length}`);
 });
