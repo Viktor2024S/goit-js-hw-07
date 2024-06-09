30fbe64d272e14d589a667194f8f89eb5b2edb95
Viktor Momotenko <viktor.momotenko@gmail.com>
2024-06-09 14:58:14 +0100

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyColor = document.querySelector("body");
+const byttonColor = document.querySelector(".change-color");
+const spanColor = document.querySelector(".color");
+
+changeColorBody = addEventListener("click", () => {
+  const color = getRandomHexColor();
+  bodyColor.style.backgroundColor = color;
+  spanColor.textContent = color;
+});
